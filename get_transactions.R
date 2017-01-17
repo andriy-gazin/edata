8033f090d23df52e87fcc2f9b9f001858100c1b4
Anatoly Bondarenko <devrand@gmail.com>
2017-01-17 18:42:01 +0200

    function sig and return added,  API param @recipt_edrpous corrected

--- get_transactions.R
+++ get_transactions.R
@@ -1,11 +1,18 @@
 library(httr)
 library(plyr)
 
-get_transactions <- function(payer, recipient = NULL, start = NULL, end = NULL) {
+#
+# function to get a transactions data from e-data.gov.ua API
+# at least one of the args, @payer OR @recipient, must be not NULL 
+# @start and @end  must be a date with  dd-mm-yyyy format
+# 
+#
+
+get_transactions <- function(payer=NULL, recipient = NULL, start = NULL, end = NULL) {
   
   request <- POST(url = "http://api.e-data.gov.ua:8080/api/rest/1.0/transactions", 
                   body = list(payers_edrpous = payer, 
-                              receipt_edrpous = recipient,
+                              recipt_edrpous = recipient,
                               startdate = start,
                               enddate = end),
                   encode = "json")
@@ -20,9 +27,12 @@ get_transactions <- function(payer, recipient = NULL, start = NULL, end = NULL) 
     
     transactions <- response$response$transactions
     transactions <- ldply(transactions, data.frame)
-
+    
+    return(transactions)
     
     }
+  
+  
 }
 
 
